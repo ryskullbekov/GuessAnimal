0ca59dab54a7a1348417b04f6dcf01d4cede662f
ryskullbekov <bpench@bk.ru>
2021-09-20 22:14:05 +0600

    Доп

--- app/src/main/java/com/example/guessanimal/MainActivity.java
+++ app/src/main/java/com/example/guessanimal/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.guessanimal;
 
+import static com.example.guessanimal.R.anim.jump_anime;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.StackView;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button dog,cat,squirrel;
     boolean isDog,isCat,isSquirrel;
+    CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +37,38 @@ public class MainActivity extends AppCompatActivity {
         dog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               if(isDog){
-                    isCat = false;
-                    isSquirrel = false;
-                    Toast.makeText(MainActivity.this, "Верно", Toast.LENGTH_SHORT).show();
+               if(isDog) {
+                   countDownTimer = new CountDownTimer(4000,1000) {
+                       @Override
+                       public void onTick(long l) {
+                           Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.jump_anime);
+                           dog.startAnimation(animation);
+                           dog.setBackgroundResource(R.drawable.correct_answer);
+                       }
+
+                       @Override
+                       public void onFinish() {
+                           dog.setBackgroundResource(R.drawable.def_answer);
+                       }
+                   }.start();
+                   Toast.makeText(MainActivity.this, "Верно", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                   countDownTimer = new CountDownTimer(4000,1000) {
+                       @Override
+                       public void onTick(long l) {
+                           YoYo.with(Techniques.Tada)
+                                   .duration(400)
+                                   .repeat(1)
+                                   .playOn(findViewById(R.id.cat_btn));
+                           dog.setBackgroundResource(R.drawable.incorrect_answer);
+                       }
+
+                       @Override
+                       public void onFinish() {
+                           dog.setBackgroundResource(R.drawable.def_answer);
+                       }
+                   }.start();
                     Toast.makeText(MainActivity.this, "Не верно", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -43,11 +78,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(isCat){
-                    isDog = false;
-                    isSquirrel = false;
+                    countDownTimer = new CountDownTimer(4000,1000) {
+                        @Override
+                        public void onTick(long l) {
+                            Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.jump_anime);
+                            cat.startAnimation(animation);
+                            cat.setBackgroundResource(R.drawable.correct_answer);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            cat.setBackgroundResource(R.drawable.def_answer);
+                        }
+                    }.start();
                     Toast.makeText(MainActivity.this, "Верно", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    countDownTimer = new CountDownTimer(4000,1000) {
+                        @Override
+                        public void onTick(long l) {
+                            YoYo.with(Techniques.Tada)
+                                    .duration(400)
+                                    .repeat(1)
+                                    .playOn(findViewById(R.id.cat_btn));
+                            cat.setBackgroundResource(R.drawable.incorrect_answer);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            cat.setBackgroundResource(R.drawable.def_answer);
+                        }
+                    }.start();
                     Toast.makeText(MainActivity.this, "Не верно", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -57,11 +118,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(isSquirrel){
-                    isDog = false;
-                    isCat = false;
+                    countDownTimer = new CountDownTimer(4000,1000) {
+                        @Override
+                        public void onTick(long l) {
+                            Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.jump_anime);
+                            squirrel.startAnimation(animation);
+                            squirrel.setBackgroundResource(R.drawable.correct_answer);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            squirrel.setBackgroundResource(R.drawable.def_answer);
+                        }
+                    }.start();
                     Toast.makeText(MainActivity.this, "Верно", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    countDownTimer = new CountDownTimer(4000,1000) {
+                        @Override
+                        public void onTick(long l) {
+                            YoYo.with(Techniques.Tada)
+                                    .duration(400)
+                                    .repeat(1)
+                                    .playOn(findViewById(R.id.squirrel_btn));
+                            squirrel.setBackgroundResource(R.drawable.incorrect_answer);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            squirrel.setBackgroundResource(R.drawable.def_answer);
+                        }
+                    }.start();
                     Toast.makeText(MainActivity.this, "Не верно", Toast.LENGTH_SHORT).show();
                 }
             }
